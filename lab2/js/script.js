d78a6693ff14ea8b2482a31056b359098126b806
Nicholas Dimitriou <skeleynick@gmail.com>
2025-09-18 17:49:46 -0700

    made changes to make able to replay

--- lab2/js/script.js
+++ lab2/js/script.js
@@ -1,7 +1,19 @@
 document.querySelector("#guessBtn").addEventListener("click", guess);
+
+document.querySelector("#reset").addEventListener("click", reset);
+
+document.querySelector("#reset").style.display = "none";
 let correctGuess = Math.floor(Math.random()*99+1);
 let numofguess = 7;
+let numGamesWon = 0;
+let NumGamesLost = 0;
+document.querySelector("#win").textContent += ""+numGamesWon;
+document.querySelector("#lost").textContent += ""+NumGamesLost;
 
+function reset(){
+    document.querySelector("#answers").textContent = "";
+    numofguess = 7;
+}
 function guess(){
     
     let userGuess = document.querySelector("#guessBox").value;
@@ -9,15 +21,19 @@ function guess(){
     document.querySelector("#answers").textContent += `${userGuess} `;
     if(numofguess > 0){
         if(userGuess>=100){
-            alert("too big a guess");
+            alert("Error: too big a guess");
         } 
         if (userGuess<=0){
-            alert("too small a guess");
+            alert("error: too small a guess");
         }
         if (userGuess == correctGuess){
             document.querySelector("#yourGuess").textContent = "Congrats, your guess was correct";
             document.querySelector("#yourGuess").style.color = "#39FF14"
+            numGamesWon++;
+            document.querySelector("#guessBtn").style.display = "none";
+            document.querySelector("#reset").style.display = "inline";
         }
+
         if(userGuess > correctGuess){
             document.querySelector("#yourGuess").textContent = "Your guess was too high";
             document.querySelector("#yourGuess").style.color = "yellow"
@@ -28,8 +44,12 @@ function guess(){
         }
     }
     if(numofguess <= 0){
-       document.querySelector("#yourGuess").textContent = "You lose"
+       document.querySelector("#yourGuess").textContent = "You lose: "
+       document.querySelector("#yourGuess").textContent += correctGuess;
        document.querySelector("#yourGuess").style.color = "red"
+        NumGamesLost++;
+        document.querySelector("#guessBtn").style.display = "none";
+         document.querySelector("#reset").style.display = "inline";
        if(userGuess == correctGuess){
             document.querySelector("#yourGuess").textContent = "Correct you still lost";
             document.querySelector("#yourGuess").style.color = "red"
---
+++ lab3/js/script.js
@@ -0,0 +1,93 @@
+document.querySelector("button").addEventListener("click", gradeQuiz);
+
+//displayQ3Options();
+function gradeQuiz(){
+    let userAnswer1 = document.querySelector("input[name=q1]:checked").value;
+    let userAnswer2 = document.querySelector("#Base").value; 
+    let userAnswer3 = document.querySelector("#selection").value;
+    let userAnswer4 = document.querySelector("#tBx").value;
+    let userAnswer5a;
+    let userAnswer5b;
+    if (document.querySelector("#q4a").checked) {
+        userAnswer5a = document.querySelector("#q4a").value;
+    }
+    if (document.querySelector("#q4c").checked) {
+        userAnswer5b = document.querySelector("#q4c").value;
+    }
+    let score = 0;
+    if(userAnswer1 == 8){
+        let answr1 = document.querySelector("#answer1")
+        answr1.textContent = "Question One is correct"
+        answr1.style.color = "green"
+        score += 20;
+    }else{
+        let answr1 = document.querySelector("#answer1")
+        answr1.textContent = "Question One is incorrect"
+        answr1.style.color = "red"
+    }
+    if(userAnswer2 == 2){
+        let answr1 = document.querySelector("#answer2")
+        answr1.textContent = "Question Two is correct"
+        answr1.style.color = "green"
+        score += 20;
+    }else{
+        let answr1 = document.querySelector("#answer2")
+        answr1.textContent = "Question Two is incorrect"
+        answr1.style.color = "red"
+    }
+    if(userAnswer3 == 1000){
+        let answr1 = document.querySelector("#answer3")
+        answr1.textContent = "Question Three is correct"
+        answr1.style.color = "green"
+        
+        score += 20;
+    }else{let answr1 = document.querySelector("#answer3")
+        answr1.textContent = "Question Three is incorrect"
+        answr1.style.color = "red"
+    }
+    if(userAnswer4 == 1011){let answr1 = document.querySelector("#answer4")
+        answr1.textContent = "Question Four is correct"
+        answr1.style.color = "green"
+        score += 20;
+    }else{
+       let answr1 = document.querySelector("#answer4")
+        answr1.textContent = "Question Four is incorrect"
+        answr1.style.color = "red"
+    }
+    if(userAnswer5a == "Used in CS" && userAnswer5b == "Can be switched to any number in base 10"){
+        let answr1 = document.querySelector("#answer5")
+        answr1.textContent = "Question Five is correct"
+        answr1.style.color = "green"
+        score += 20;
+    }else{
+       let answr1 = document.querySelector("#answer5")
+        answr1.textContent = "Question Five is incorrect"
+        answr1.style.color = "red"
+    }
+    let answr1 = document.querySelector("#score")
+        answr1.textContent = score;
+}
+
+// function displayQ3Options(){
+
+//     let q3Options = ["font-color", "fontColor", "color", "textColor"];
+//     q3Options = _.shuffle(q3Options);
+
+//     for (let i of q3Options) {
+
+//         let inputElement = document.createElement("input");
+//         inputElement.type = "radio";
+//         inputElement.name = "q3";
+//         inputElement.value = i;
+//         console.log(inputElement);
+
+//         let labelElement = document.createElement("label");
+//         labelElement.textContent = i;
+//         labelElement.prepend(inputElement);
+
+//         document.querySelector("#q3Options").append(labelElement);
+        
+//     }
+
+
+// }
