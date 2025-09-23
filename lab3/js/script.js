158f42d9d35177bad0ddcac1d3097af85e171513
Nicholas Dimitriou <skeleynick@gmail.com>
2025-09-22 17:41:32 -0700

    everything but css

--- lab3/js/script.js
+++ lab3/js/script.js
@@ -1,4 +1,23 @@
 document.querySelector("button").addEventListener("click", gradeQuiz);
+let Onec = document.getElementById("img1c");
+let Onew = document.getElementById("img1w");
+let Twoc = document.getElementById("img2c");
+let Twow = document.getElementById("img2w");
+let Threec = document.getElementById("img3c");
+let Threew = document.getElementById("img3w");
+let Fourc = document.getElementById("img4c");
+let Fourw = document.getElementById("img4w");
+let Fivec = document.getElementById("img5c");
+let Fivew = document.getElementById("img5w");
+let dateTime = new Date();
+let hr = dateTime.getHours();
+let min = dateTime.getMinutes();
+let sec = dateTime.getSeconds();
+localStorage.setItem("hr", hr);
+localStorage.setItem("min", min);
+localStorage.setItem("sec", sec);
+
+
 
 displayQ3Options();
 function gradeQuiz(){
@@ -9,6 +28,23 @@ function gradeQuiz(){
     let userAnswer4 = document.querySelector("#tBx").value;
     let userAnswer5a;
     let userAnswer5b;
+    let date = new Date();
+    let nhr = date.getHours();
+    let nmin = date.getMinutes();
+    let nsec = date.getSeconds();
+    localStorage.getItem("hr");
+    localStorage.getItem("min");
+    localStorage.getItem("sec");
+
+    let total = document.querySelector("#totalTime");
+    let chr = localStorage.getItem("hr");
+    let cmin = localStorage.getItem("min");
+    let csec = localStorage.getItem("sec");
+    let totalhr = nhr - chr; 
+    let totalmin = nmin - cmin;
+    let totalsec = nsec - csec;
+    total.textContent += totalhr + "hrs " + totalmin + "mins "+totalsec+"secs";
+
     if (document.querySelector("#q4a").checked) {
         userAnswer5a = document.querySelector("#q4a").value;
     }
@@ -19,54 +55,66 @@ function gradeQuiz(){
     if(userAnswer1 == 8){
         let answr1 = document.querySelector("#answer1")
         answr1.textContent = "Question One is correct"
-        answr1.style.color = "green"
+        answr1.style.color = "rgb(0, 255, 0)"
+        Onec.removeAttribute("hidden");
+
         score += 20;
     }else{
         let answr1 = document.querySelector("#answer1")
         answr1.textContent = "Question One is incorrect"
         answr1.style.color = "red"
+        Onew.removeAttribute("hidden")
     }
     if(userAnswer2 == 2){
         let answr1 = document.querySelector("#answer2")
         answr1.textContent = "Question Two is correct"
-        answr1.style.color = "green"
+        answr1.style.color = "rgb(0, 255, 0)"
+        Twoc.removeAttribute("hidden");
         score += 20;
     }else{
         let answr1 = document.querySelector("#answer2")
         answr1.textContent = "Question Two is incorrect"
         answr1.style.color = "red"
+        Twow.removeAttribute("hidden");
     }
     if(userAnswer3 == 1000){
         let answr1 = document.querySelector("#answer3")
         answr1.textContent = "Question Three is correct"
-        answr1.style.color = "green"
+        answr1.style.color = "rgb(0, 255, 0)"
+        Threec.removeAttribute("hidden");
         
         score += 20;
     }else{let answr1 = document.querySelector("#answer3")
         answr1.textContent = "Question Three is incorrect"
         answr1.style.color = "red"
+        Threew.removeAttribute("hidden");
     }
     if(userAnswer4 == 1011){let answr1 = document.querySelector("#answer4")
         answr1.textContent = "Question Four is correct"
-        answr1.style.color = "green"
+        answr1.style.color = "rgb(0, 255, 0)"
+        Fourc.removeAttribute("hidden");
+        
         score += 20;
     }else{
        let answr1 = document.querySelector("#answer4")
         answr1.textContent = "Question Four is incorrect"
         answr1.style.color = "red"
+        Fourw.removeAttribute("hidden");
     }
     if(userAnswer5a == "Used in CS" && userAnswer5b == "Can be switched to any number in base 10"){
         let answr1 = document.querySelector("#answer5")
         answr1.textContent = "Question Five is correct"
-        answr1.style.color = "green"
+        answr1.style.color = "rgb(0, 255, 0)"
+        Fivec.removeAttribute("hidden");
         score += 20;
     }else{
        let answr1 = document.querySelector("#answer5")
         answr1.textContent = "Question Five is incorrect"
         answr1.style.color = "red"
+        Fivew.removeAttribute("hidden");
     }
     let answr1 = document.querySelector("#score")
-        answr1.textContent = score;
+        answr1.textContent +=" "+ score;
     if(score >= 80){
         congrats.textContent += "Congrats on the great score";
         congrats.style.color = "green";
